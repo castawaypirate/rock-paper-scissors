ce64f3f6e8ddc1521ee0c5e9b45563e4df8df594
castaway <apsikiotis@gmail.com>
2023-11-22 16:04:19 +0200

    Change comment

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- * The value is no lower than min (or the next integer greater than min
- * if min isn't an integer) and no greater than max (or the next integer
- * lower than max if max isn't an integer).
- * Using Math.round() will give you a non-uniform distribution!
- */
+//Returns a random integer in a range
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
